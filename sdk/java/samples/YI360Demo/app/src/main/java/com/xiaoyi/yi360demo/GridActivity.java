abb9938577466d0a5aa3e1c0cc90bc3ced3502e1
Jen McKenzie <jennifermckenzie@gmail.com>
2017-05-21 22:09:59 +1000

    Added settings for photo and video

--- sdk/java/samples/YI360Demo/app/src/main/java/com/xiaoyi/yi360demo/GridActivity.java
+++ sdk/java/samples/YI360Demo/app/src/main/java/com/xiaoyi/yi360demo/GridActivity.java
@@ -20,12 +20,17 @@ package com.xiaoyi.yi360demo;
 import android.content.DialogInterface;
 import android.net.wifi.WifiManager;
 import android.os.*;
+import android.print.PrintAttributes;
 import android.support.v7.app.*;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridView;
+import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.Switch;
+
 import java.net.*;
 import java.io.*;
 import java.lang.reflect.Method;
@@ -35,6 +40,17 @@ import java.util.Timer;
 import java.util.TimerTask;
 import com.xiaoyi.action.*;
 
+enum ControlView {
+    cameraSelection,
+    cameraGrid,
+    settings
+};
+
+enum CameraMode {
+    video,
+    photo
+}
+
 public class GridActivity extends AppCompatActivity {
     private boolean mStartScanTimer;
     private GridView gridView;
@@ -43,6 +59,34 @@ public class GridActivity extends AppCompatActivity {
     public ArrayList<Camera> pendingCameras;
     public ArrayList<Camera> cameras;
     public GridAdapter gridAdapter;
+    private CameraMode cameraMode = CameraMode.video;
+    private boolean isRecording = false;
+
+    // Photo settings
+    private ColorMode photoColorMode = ColorMode.YIColor;
+    private ExposureValue photoExposureValue = ExposureValue.ev_0;
+    private ISO photoIso = ISO.iso_Auto;
+    private MeteringMode photoMeteringMode = MeteringMode.Center;
+    private PhotoResolution photoResolution = PhotoResolution.p_12MP_4000x3000_4x3_w;
+    private Sharpness photoSharpness = Sharpness.Medium;
+    private ShutterTime photoShutterTime = ShutterTime.st_Auto;
+    private WhiteBalance photoWhiteBalance = WhiteBalance.Auto;
+
+    // Video settings
+    private ColorMode videoColorMode = ColorMode.YIColor;
+    private ExposureValue videoExposureValue = ExposureValue.ev_0;
+    private ISO videoIso = ISO.iso_Auto;
+    private FieldOfView videoFieldOfView = FieldOfView.Medium;
+    private MeteringMode videoMeteringMode = MeteringMode.Center;
+    private VideoResolution videoResolution = VideoResolution.v_3840x2160_30p_16x9_super;
+    private Sharpness videoSharpness = Sharpness.Medium;
+    private Quality videoQuality = Quality.High;
+    private VideoStandard videoStandard = VideoStandard.PAL;
+    private WhiteBalance videoWhiteBalance = WhiteBalance.Auto;
+    private VideoRotateMode videoRotate = VideoRotateMode.Auto;
+    private ToggleState videoMuteState = ToggleState.Off;
+    private int videoDelayStart = 10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +128,9 @@ public class GridActivity extends AppCompatActivity {
         setContentView(R.layout.grid_activity);
         gridView = (GridView) findViewById(R.id.gridView);
         gridView.setAdapter(gridAdapter);
+
+        updateActionButtonText();
+        initSettings();
     }
 
     @Override
@@ -100,7 +147,11 @@ public class GridActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (mExitDialog != null && mExitDialog.isShowing()) {
+        if (findViewById(R.id.photoSettings).getVisibility() == View.VISIBLE ||
+            findViewById(R.id.videoSettings).getVisibility() == View.VISIBLE) {
+            updateSettings();
+            showControlView(ControlView.cameraGrid);
+        } else if (mExitDialog != null && mExitDialog.isShowing()) {
             mExitDialog.dismiss();
         } else {
             exit(null);
@@ -132,18 +183,34 @@ public class GridActivity extends AppCompatActivity {
         return null;
     }
 
-    public void startRecording(MenuItem item) {
+    private void startRecording() {
+        setRecording(true);
+
         // Start recording after 10 seconds
         Date date = new Date();
-        date.setTime(date.getTime() + 10 * 1000);
+        date.setTime(date.getTime() + videoDelayStart * 1000);
         for (Camera camera: cameras) {
             if (!camera.getIp().isEmpty()) {
-                camera.startRecording(date);
+                camera.startRecording(
+                        date,
+                        videoColorMode,
+                        videoExposureValue,
+                        videoIso,
+                        videoFieldOfView,
+                        videoMeteringMode,
+                        videoResolution,
+                        videoQuality,
+                        videoSharpness,
+                        videoWhiteBalance,
+                        videoStandard,
+                        videoMuteState);
             }
         }
     }
 
-    public void stopRecording(MenuItem item) {
+    private void stopRecording() {
+        setRecording(false);
+
         for (Camera camera: cameras) {
             if (!camera.getIp().isEmpty()) {
                 camera.stopRecording();
@@ -151,12 +218,30 @@ public class GridActivity extends AppCompatActivity {
         }
     }
 
-    public void capturePhoto(MenuItem item) {
+    private void capturePhoto() {
         for (Camera camera: cameras) {
             if (!camera.getIp().isEmpty()) {
-                camera.capturePhoto();
+                camera.capturePhoto(
+                        photoColorMode,
+                        photoExposureValue,
+                        photoIso,
+                        photoMeteringMode,
+                        photoResolution,
+                        photoSharpness,
+                        photoShutterTime,
+                        photoWhiteBalance);
             }
         }
+    }
+
+    public void showSettings(MenuItem item) {
+        showControlView(ControlView.settings);
+    }
+
+    public void switchMode(MenuItem item) {
+        stopRecording();
+        cameraMode = cameraMode == CameraMode.video ? CameraMode.photo : CameraMode.video;
+        updateActionButtonText();
     }
 
     public void exit(MenuItem item) {
@@ -168,7 +253,7 @@ public class GridActivity extends AppCompatActivity {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     mStartScanTimer = false;
-                    stopRecording(null);
+                    stopRecording();
                     for (Camera camera : cameras) {
                         camera.disconnect();
                     }
@@ -208,8 +293,7 @@ public class GridActivity extends AppCompatActivity {
             mMenu.getItem(i).setVisible(true);
         }
 
-        findViewById(R.id.cameraSelectionView).setVisibility(View.INVISIBLE);
-        findViewById(R.id.cameraGridView).setVisibility(View.VISIBLE);
+        showControlView(ControlView.cameraGrid);
 
         cameras.clear();
         for (int i = 0; i < cameraCount; ++i) {
@@ -322,5 +406,105 @@ public class GridActivity extends AppCompatActivity {
 
             }
         }).show();
+    }
+
+    private void showControlView(ControlView view) {
+        findViewById(R.id.cameraSelectionView).setVisibility(view == ControlView.cameraSelection ? View.VISIBLE : View.INVISIBLE);
+        findViewById(R.id.cameraGridView).setVisibility(view == ControlView.cameraGrid ? View.VISIBLE : View.INVISIBLE);
+        findViewById(R.id.photoSettings).setVisibility(view == ControlView.settings && cameraMode == CameraMode.photo ? View.VISIBLE : View.INVISIBLE);
+        findViewById(R.id.videoSettings).setVisibility(view == ControlView.settings && cameraMode == CameraMode.video ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    public void actionButtonPressed(View view) {
+        if (cameraMode == CameraMode.video) {
+            if (isRecording) {
+                stopRecording();
+            } else {
+                startRecording();
+            }
+        } else {
+            capturePhoto();
+        }
+    }
+
+    private void setRecording(boolean recording) {
+        isRecording = recording;
+        updateActionButtonText();
+    }
+
+    private void updateActionButtonText() {
+        Button actionButton = (Button) findViewById(R.id.actionButton);
+        actionButton.setText(cameraMode == CameraMode.video ? (isRecording ? "Stop Recording" : "Start Recording") : "Capture Photograph");
+    }
+
+    private void updateSettings() {
+        photoColorMode = ColorMode.values()[((Spinner) findViewById(R.id.photoColorMode)).getSelectedItemPosition() + 1];
+        photoExposureValue = ExposureValue.values()[((Spinner) findViewById(R.id.photoExposureValue)).getSelectedItemPosition() + 1];
+        photoIso = ISO.values()[((Spinner) findViewById(R.id.photoIso)).getSelectedItemPosition() + 1];
+        photoMeteringMode = MeteringMode.values()[((Spinner) findViewById(R.id.photoMeteringMode)).getSelectedItemPosition() + 1];
+        photoResolution = PhotoResolution.values()[((Spinner) findViewById(R.id.photoResolution)).getSelectedItemPosition() + 1];
+        photoSharpness = Sharpness.values()[((Spinner) findViewById(R.id.photoSharpness)).getSelectedItemPosition() + 1];
+        photoShutterTime = ShutterTime.values()[((Spinner) findViewById(R.id.photoShutterTime)).getSelectedItemPosition() + 1];
+        photoWhiteBalance = WhiteBalance.values()[((Spinner) findViewById(R.id.photoWhiteBalance)).getSelectedItemPosition() + 1];
+
+        videoColorMode = ColorMode.values()[((Spinner) findViewById(R.id.videoColorMode)).getSelectedItemPosition() + 1];
+        videoExposureValue = ExposureValue.values()[((Spinner) findViewById(R.id.videoExposureValue)).getSelectedItemPosition() + 1];
+        videoIso = ISO.values()[((Spinner) findViewById(R.id.videoIso)).getSelectedItemPosition() + 1];
+        videoFieldOfView = FieldOfView.values()[((Spinner) findViewById(R.id.videoFieldOfView)).getSelectedItemPosition() + 1];
+        videoMeteringMode = MeteringMode.values()[((Spinner) findViewById(R.id.videoMeteringMode)).getSelectedItemPosition() + 1];
+        videoResolution = VideoResolution.values()[((Spinner) findViewById(R.id.videoResolution)).getSelectedItemPosition() + 1];
+        videoSharpness = Sharpness.values()[((Spinner) findViewById(R.id.videoSharpness)).getSelectedItemPosition() + 1];
+        videoQuality = Quality.values()[((Spinner) findViewById(R.id.videoQuality)).getSelectedItemPosition() + 1];
+        videoStandard = VideoStandard.values()[((Spinner) findViewById(R.id.videoStandard)).getSelectedItemPosition() + 1];
+        videoWhiteBalance = WhiteBalance.values()[((Spinner) findViewById(R.id.videoWhiteBalance)).getSelectedItemPosition() + 1];
+        videoRotate = VideoRotateMode.values()[((Spinner) findViewById(R.id.videoRotate)).getSelectedItemPosition() + 1];
+        videoMuteState = ((Switch) findViewById(R.id.videoMuteState)).isChecked() ? ToggleState.On : ToggleState.Off;
+
+        int position = ((Spinner) findViewById(R.id.videoStartDelay)).getSelectedItemPosition();
+        switch(position) {
+            case 0: videoDelayStart = 0; break;
+            case 1: videoDelayStart = 1; break;
+            case 2: videoDelayStart = 5; break;
+            case 3: videoDelayStart = 10; break;
+            case 4: videoDelayStart = 15; break;
+            case 5: videoDelayStart = 30; break;
+        }
+    }
+
+    private void initSettings() {
+
+        ((Spinner) findViewById(R.id.photoColorMode)).setSelection(photoColorMode.ordinal() - 1);
+        ((Spinner) findViewById(R.id.photoExposureValue)).setSelection(photoExposureValue.ordinal() - 1);
+        ((Spinner) findViewById(R.id.photoIso)).setSelection(photoIso.ordinal() - 1);
+        ((Spinner) findViewById(R.id.photoMeteringMode)).setSelection(photoMeteringMode.ordinal() - 1);
+        ((Spinner) findViewById(R.id.photoResolution)).setSelection(photoResolution.ordinal() - 1);
+        ((Spinner) findViewById(R.id.photoSharpness)).setSelection(photoSharpness.ordinal() - 1);
+        ((Spinner) findViewById(R.id.photoShutterTime)).setSelection(photoShutterTime.ordinal() - 1);
+        ((Spinner) findViewById(R.id.photoWhiteBalance)).setSelection(photoWhiteBalance.ordinal() - 1);
+
+        ((Spinner) findViewById(R.id.videoColorMode)).setSelection(videoColorMode.ordinal() - 1);
+        ((Spinner) findViewById(R.id.videoExposureValue)).setSelection(videoExposureValue.ordinal() - 1);
+        ((Spinner) findViewById(R.id.videoIso)).setSelection(videoIso.ordinal() - 1);
+        ((Spinner) findViewById(R.id.videoFieldOfView)).setSelection(videoFieldOfView.ordinal() - 1);
+        ((Spinner) findViewById(R.id.videoMeteringMode)).setSelection(videoMeteringMode.ordinal() - 1);
+        ((Spinner) findViewById(R.id.videoResolution)).setSelection(videoResolution.ordinal() - 1);
+        ((Spinner) findViewById(R.id.videoSharpness)).setSelection(videoSharpness.ordinal() - 1);
+        ((Spinner) findViewById(R.id.videoQuality)).setSelection(videoQuality.ordinal() - 1);
+        ((Spinner) findViewById(R.id.videoStandard)).setSelection(videoStandard.ordinal() - 1);
+        ((Spinner) findViewById(R.id.videoWhiteBalance)).setSelection(videoWhiteBalance.ordinal() - 1);
+        ((Spinner) findViewById(R.id.videoRotate)).setSelection(videoRotate.ordinal() - 1);
+        ((Switch) findViewById(R.id.videoMuteState)).setChecked(videoMuteState == ToggleState.On);
+
+        int position = 10;
+        switch(videoDelayStart) {
+            case 0: position = 0; break;
+            case 1: position = 1; break;
+            case 5: position = 2; break;
+            case 10: position = 3; break;
+            case 15: position = 4; break;
+            case 30: position = 5; break;
+        }
+        ((Spinner) findViewById(R.id.videoStartDelay)).setSelection(position);
+
     }
 }
